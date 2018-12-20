9febaafc38760a78252b420fc6cd36fe3161a36f
23nfl <23nfl@DESKTOP-DGNSB50.attlocal.net>
2018-12-20 06:34:07 -0800

    SeaCreature

---
+++ src/SeaCreatureRunner.java
@@ -0,0 +1,18 @@
+
+public class SeaCreatureRunner {
+public static void main(String[] args) {
+SeaCreature s=new SeaCreature("spongebob");
+SeaCreature squid=new SeaCreature("squidward");
+SeaCreature p=new SeaCreature("patrick");
+s.getName();
+s.eat();
+s.laugh();
+squid.getName();
+squid.eat();
+squid.laugh();
+p.getName();
+p.eat();
+p.laugh();
+
+}
+}
