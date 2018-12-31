172f5e38106e3ee05475e2693f504c8805ce6756
23nfl <23nfl@DESKTOP-DGNSB50.attlocal.net>
2018-12-31 14:48:27 -0800

    Popcorn

--- src/PopcornMaker.java
+++ src/PopcornMaker.java
@@ -3,8 +3,14 @@ import javax.swing.JOptionPane;
 public class PopcornMaker {
 public static void main(String[] args) {
 Microwave m=new Microwave();
-int flavor=Integer.parseInt(JOptionPane.showInputDialog("What flavor popcorn do you want?"));
 int cookTime=Integer.parseInt(JOptionPane.showInputDialog("How many minutes do you want to cook the popcorn?"));
+String flavor=JOptionPane.showInputDialog("What flavor popcorn do you want?");
 m.putInMicrowave(null);
+m.setTime(cookTime);
+m.startMicrowave();
+Popcorn p=new Popcorn(flavor);
+p.applyHeat();
+p.eat();
 }
 }
+
